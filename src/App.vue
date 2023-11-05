e436f127c66fdecda7691cb499a08b14404618b3
hair-cut <kikl003273@naver.com>
2023-11-05 19:19:25 +0900

    bootstrap문제

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <body>
+        <div>
+            <b-button>Button</b-button>
+            <b-button variant="danger">Button</b-button>
+            <b-button variant="success">Button</b-button>
+            <b-button variant="outline-primary">Button</b-button>
+        </div>
+    </body>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        background-color: black;
+        color: white;
+        /* 텍스트 색상을 흰색으로 설정 */
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
+import router from './router'
+import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+// vue3는 bootstrap 못써서 vue2 검토해야함
+const app = createApp(App)
 createApp(App).mount('#app')
+app.use(BootstrapVue3)
+app.use(router)
+app.mount('#app')
